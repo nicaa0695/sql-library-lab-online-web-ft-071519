8fe02384298dac808541c64f006db64e94941a17
Veronica Lopez <nicaa0695@gmail.com>
2019-08-15 21:40:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre FROM authors"
+  "SELECT authors.name, series.subgenre_id FROM authors"
 end
 
 def select_series_title_with_most_human_characters
